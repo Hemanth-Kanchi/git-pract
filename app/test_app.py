0330f6932ea93e0fd170c120a26e00e64d7f47b5
Hemanth-Kanchi <kanchi.hemanth@rsystems.com>
2026-07-15 10:39:39 +0000

    changed version of api from the app.test

--- app/test_app.py
+++ app/test_app.py
@@ -4,7 +4,7 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json["version"] == "v2"
+    assert response.json["version"] == "v1"
 
 def test_health():
     client = app.test_client()
